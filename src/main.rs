df899c5f054db340273ec9ea4351eb81afc5855a
Narek Asadorian <nasadorian@salesforce.com>
2022-04-03 17:59:28 -0700

    remove errors

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,8 @@ mod db;
 mod logger;
 mod models;
 mod routes;
-mod errors;
 
 type StdErr = Box<dyn std::error::Error>;
-
-#[actix_web::get("/")]
-async fn hello_world() -> &'static str {
-    "Hello, world!"
-}
 
 #[actix_web::main]
 async fn main() -> Result<(), StdErr> {
@@ -21,7 +15,7 @@ async fn main() -> Result<(), StdErr> {
     actix_web::HttpServer::new(move || {
         actix_web::App::new()
             .data(db.clone())
-            .service(hello_world)
+            .service(routes::health_check)
             .service(routes::api())
     })
     .bind(("127.0.0.1", 8080))?
