307b702e0dba144da619ba79e2e7721def9e14bc
Narek Asadorian <nasadorian@salesforce.com>
2022-05-03 22:57:02 -0700

    add contract file

--- src/contract.rs
+++ src/contract.rs
@@ -5,6 +5,10 @@ use ethers::types::transaction::eip712::*;
 use ethers::core::k256::ecdsa::recoverable;
 use serde::Serialize;
 use std::string::FromUtf8Error;
+<<<<<<< Updated upstream
+=======
+use std::env;
+>>>>>>> Stashed changes
 
 #[derive(Serialize)]
 pub enum NFTError {
@@ -41,9 +45,14 @@ impl From<WalletError> for NFTError {
 #[eip712(
     name = "DBio",
     version = "1",
+<<<<<<< Updated upstream
     chain_id = 1337, // Rinkeby testnet
     //verifying_contract = "1D3A4476FF9502F73D5b691f7B51dD6b79D8aF18"
     verifying_contract = "0x7B1B344482183CF8D06540E9429ac418c928e28a"
+=======
+    chain_id = 1, // Rinkeby testnet
+    verifying_contract = "0xEdd57d64f68D11cEF21bAacBfbcDE308DC1bF828"
+>>>>>>> Stashed changes
 )]
 struct NFTVoucher {
     uri: String,
@@ -60,10 +69,16 @@ pub struct NFTVoucherPayload {
 }
 
 pub async fn create_nft_voucher(cid: String) -> Result<NFTVoucherPayload, NFTError> {
+<<<<<<< Updated upstream
 
     let uri = to_ipfs_uri(cid);
     let wallet: LocalWallet = "137daea58403567b8ea0922ad7e89ace9acf4ade3e4add0a9d4049fa8a54a180".parse()?;
         
+=======
+    let uri = to_ipfs_uri(cid);
+    let key = env::var("ETH_PRIVATE_KEY")?;
+    let wallet: LocalWallet = key.parse()?;
+>>>>>>> Stashed changes
     let data = NFTVoucher { uri: uri.clone(), };
     println!("{}", hex::encode(data.struct_hash().unwrap()));
     let sig: recoverable::Signature = wallet.sign_typed_data(&data).await?;
